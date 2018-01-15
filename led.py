85b31854ddb741bea302e7ddbd0f6c8e9ff9b08f
Enokida1223 <34084590+Enokida1223@users.noreply.github.com>
2018-01-15 16:37:40 +0900

    Update led.py

--- led.py
+++ led.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
+import rospy
+import time
 import pigpio
 from time import sleep
 
 PIN = 25
+FREQ = 50
+RANGE = 100
 
 pi = pigpio.pi()
+
 pi.set_mode(PIN, pigpio.OUTPUT)
+pi.set_PWM_frequency(PIN, FREQ)
+pi.set_PWM_range(PIN, RANGE)
+
 
 try:
+    d = 0
+    r = 20
     while True:
-        pi.write(PIN, 1)
-        sleep(0.5)
-        pi.write(PIN, 0)
-        sleep(0.5)
+        pi.set_PWM_dutycycle(PIN, d)
+        sleep(1.5)
+        d += r
+        if d >= RANGE or d <= 0:
+            r *= -1
 
 except KeyboardInterrupt:
     pass
